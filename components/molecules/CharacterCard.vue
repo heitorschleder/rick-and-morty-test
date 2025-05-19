11bbc9bdc3b755c238f8cb86f503d172aed12d2e
Heitor <heitorschleder33@gmail.com>
2025-05-19 10:33:57 -0300

    randomizing infos in license portal card, fixing opacity in character image

--- components/molecules/CharacterCard.vue
+++ components/molecules/CharacterCard.vue
@@ -8,6 +8,45 @@ defineProps<{
   }
 }>()
 defineEmits(['click'])
+
+const addresses = [
+  "42 E Squanch Pl<br />Seattle, WA 98020",
+  "123 Rick Rd<br />Dimension C-137",
+  "7 Gazorpazorp St<br />Planet Gazorpazorp",
+  "99 Meeseeks Ln<br />Multiverse 42",
+  "101 Jerry Blvd<br />Earth (Replacement)",
+  "666 Evil Morty Ave<br />Citadel of Ricks",
+  "314 Pickle Pkwy<br />Pickleverse 3.14",
+  "1 Birdperson Blvd<br />Phoenix, AZ",
+  "404 Portal Gun Way<br />Unknown Location",
+  "999 Summer Ct<br />Dimension J19ζ7",
+  "202 Squanch Blvd<br />Planet Squanch",
+  "88 Cronenberg St<br />Post-Apocalyptic Earth",
+  "77 Glip Glop Ln<br />Blips and Chitz",
+  "12 Plumbus Way<br />Gromflomite Sector",
+  "555 Unity Dr<br />Hive Mind County",
+  "390 Wormhole Loop<br />Nebulon Cluster",
+  "808 Time Travel Rd<br />Temporal Zone 9",
+  "321 Morty Meadow<br />Dimension Replacement A",
+  "187 Toxic Rick Rd<br />Toxic World 6B",
+  "0 Null St<br />Existence Outside Time"
+]
+
+const getRandomAddress = () => {
+  const index = Math.floor(Math.random() * addresses.length)
+  return addresses[index]
+}
+
+const restrictions = ["Brine", "Tasks", "Rick", "Parasites", "Burp"]
+
+const getRandomRestriction = () => {
+  const index = Math.floor(Math.random() * restrictions.length)
+  return restrictions[index]
+}
+
+const restriction = getRandomRestriction()
+
+const address = getRandomAddress()
 </script>
 
 <template>
@@ -37,7 +76,7 @@ defineEmits(['click'])
         </div>
         <div class="flex flex-col">
           <span class="text-center">Restrictions</span>
-          <span class="text-center">Brine</span>
+          <span class="text-center">{{ restriction }}</span>
         </div>
       </div>
       <div class="flex gap-4">
@@ -45,18 +84,16 @@ defineEmits(['click'])
         <!-- Character Image -->
         <div class="w-1/3">
           <img :src="character.image" :alt="character.name"
-            class="border border-black rounded-sm w-full h-32 object-cover z-10 opacity-100" />
+            class="relative z-50 w-full h-32 object-cover border border-black rounded-sm" />
         </div>
 
-        <div class="flex flex-col ml-12">
+        <div class="flex flex-col text-center w-64">
           <!-- Name -->
-          <div class="mt-3 text-2xl font-bold text-center text-[#29778B] drop-shadow-md truncate font-portal">
+          <div class="mt-3 text-[1.9rem] font-bold text-[#29778B] drop-shadow-md truncate font-portal">
             {{ character.name }}
           </div>
-          <!-- Address (fixo ou pode ser randomizado) -->
-          <div class="text-center text-sm mt-1 font-semibold">
-            42 E Squanch Pl<br />
-            Seattle, WA 98020
+          <!-- Address -->
+          <div class="text-center text-base mt-1 font-semibold" v-html="address">
           </div>
         </div>
       </div>
