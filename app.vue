c89e685d83426538955ab69e10a050c26639bd03
Heitor <heitorschleder33@gmail.com>
2025-05-19 18:11:15 -0300

    adding metas and favicon

--- app.vue
+++ app.vue
@@ -1,3 +1,17 @@
+<script setup lang="ts">
+useHead({
+  title: 'Rick and Morty Explorer',
+  meta: [
+    { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+    { name: 'description', content: 'Explore and search characters from the Rick and Morty universe using a GraphQL-powered Nuxt 3 app.' },
+    { name: 'keywords', content: 'Rick and Morty, Nuxt 3, GraphQL, TailwindCSS, Vue, Character Search' },
+    { name: 'author', content: 'Heitor Schleder' }
+  ],
+  link: [
+    { rel: 'icon', type: 'image/png', href: '/favicon.png' }
+  ]
+})
+</script>
 <template>
   <div class="min-h-screen">
     <NuxtPage />
